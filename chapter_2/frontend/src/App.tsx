52af076aacb57e498da894382aa5f799f23c730a
Rahul T Gerorge <34202955+rahul-george@users.noreply.github.com>
2025-07-16 20:17:03 +0000

    Missed committing file.

--- chapter_2/frontend/src/App.tsx
+++ chapter_2/frontend/src/App.tsx
@@ -1,20 +1,8 @@
-import { Box, Separator} from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 import Polling from "@/section/polling/polling";
 
 function App() {
-  const [message, setMessage] = useState("Loading...");
-
-const handleFetch = async () => {
-    try {
-      const response = await api.get('/polling/logs/1');
-      setMessage(JSON.stringify(response.data, null, 2));
-      console.log('✅ Data:', response.data);
-    } catch (error) {
-      console.error('❌ Error fetching data:', error);
-      setMessage('Error fetching data');
-    }
-  };
 
   return (
     <Box p={6}>
